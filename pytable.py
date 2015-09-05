256495d8a71f2f9fe341ff54302a8682bc6c98db
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-09-05 13:09:06 +0200

    First try on alignment

--- pytable.py
+++ pytable.py
@@ -26,11 +26,12 @@ class FancyRenderer(BaseRenderer):
         self._print_divider()
 
     def print_row(self, values):
-        map(self.print_cell, values, self.widths)
+        map(self.print_cell, self.table.columns, values, self.widths)
         self.print_delimiter()
 
-    def print_cell(self, value, width):
-        self.p('| {:<{w}}'.format(value, w=width))
+    def print_cell(self, column, value, width):
+        align = column.attrs.get(ALIGN_KEY, ALIGN_LEFT)
+        self.p('| {:{align}{w}} '.format(value, w=width - 1, align=align))
 
     def print_delimiter(self):
         self.p('|\n')
@@ -39,6 +40,50 @@ class FancyRenderer(BaseRenderer):
         for w in self.widths:
             self.p('+' + '-' * (w + 1))
         self.p('+\n')
+
+
+def set_attr(table, key, value):
+    """
+    Set an attribute on all columns of a table.
+
+    :param key: name of the attribute
+    :param value: value of the attribute
+    :returns: new table
+    """
+    def worker(c, k, v):
+        c.attrs[k] = v
+        return c
+    return Table(tuple(worker(col, key, value) for col in table.columns[:]))
+
+
+ALIGN_KEY = 'attr_align'
+ALIGN_LEFT = '<'
+ALIGN_RIGHT = '>'
+ALIGN_CENTER = '^'
+
+
+def align_left(table):
+    """
+    Creates a new table with the same columns as the given table. All columns
+    in new table are left aligned.
+    """
+    return set_attr(table, ALIGN_KEY, ALIGN_LEFT)
+
+
+def align_right(table):
+    """
+    Creates a new table with the same columns as the given table. All columns
+    in new table are right aligned.
+    """
+    return set_attr(table, ALIGN_KEY, ALIGN_RIGHT)
+
+
+def align_center(table):
+    """
+    Creates a new table with the same columns as the given table. All columns
+    in new table are center aligned.
+    """
+    return set_attr(table, ALIGN_KEY, ALIGN_CENTER)
 
 
 class HtmlRenderer(BaseRenderer):
@@ -87,6 +132,7 @@ class Column(object):
     def __init__(self, header, cell):
         self.header = header
         self.cell = cell
+        self.attrs = {}
 
     def max_width(self, data):
         data_max = 0
@@ -122,6 +168,10 @@ def stringable(header, getter):
     return column(header, wrapped_getter)
 
 
+def integer(header, getter):
+    return align_right(stringable(header, getter))
+
+
 def boolean(header, getter, mapping=None):
     """
     Create a table consisting of a single column. The getter must return a
@@ -144,12 +194,12 @@ def boolean(header, getter, mapping=None):
 
 if __name__ == '__main__':
     from operator import itemgetter
-    table = (stringable('X', itemgetter('x')) +
-             stringable('Y', itemgetter('y')) +
-             column('Name', itemgetter('name')))
+    table = (integer('X', itemgetter('x')) +
+             integer('Y', itemgetter('y')) +
+             align_center(column('Name', itemgetter('name'))))
     data = [
         {'x': 0, 'y': 0, 'name': 'Origin'},
         {'x': 5, 'y': 5, 'name': 'Diagonal'},
-        {'x': 2, 'y': 8, 'name': 'Up'},
+        {'x': 12, 'y': 8, 'name': 'Up'},
     ]
     table.render(data, renderer=FancyRenderer)
